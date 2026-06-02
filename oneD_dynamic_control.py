06ea881c0703b7b6b2a89134d633bcd05db652c7
Jacquie Groenewegen <21jng3@queensu.ca>
2026-06-02 11:31:55 -0400

    added more examples

--- oneD_dynamic_control.py
+++ oneD_dynamic_control.py
@@ -1,9 +1,10 @@
 """
-Example oneD_kinematic_control.py
+Example oneD_dynamic_control.py
 Author: Joshua A. Marshall <joshua.marshall@queensu.ca>
 GitHub: https://github.com/botprof/agv-examples
 """
 
+# %%
 # SIMULATION SETUP
 
 import numpy as np
@@ -11,7 +12,7 @@ import matplotlib.pyplot as plt
 from mobotpy.models import Cart
 
 # Set the simulation time [s] and the sample period [s]
-SIM_TIME = 10.0
+SIM_TIME = 30.0
 T = 0.04
 
 # Create an array of time values [s]
@@ -21,62 +22,67 @@ N = np.size(t)
 # %%
 # FUNCTION DEFINITIONS
 
+# Set the mass of the vehicle [kg]
+m = 10.0
 
-def vehicle(x, u, T):
-    """Discrete-time 1D kinematic vehicle model."""
-    x_new = x + T * u
+# Define the vehicle model matrices
+F = np.array([[1, T], [0, 1]])
+G = np.array([[T**2 / (2 * m)], [T / m]])
+
+
+def vehicle(x, u, F, G):
+    """Discrete-time 1D dynamic vehicle model."""
+    x_new = F @ x + G @ [u]
     return x_new
+
+
+def controller(x, K):
+    """Proportional controller."""
+    u_new = -K @ x
+    return u_new
 
 
 # %%
 # RUN SIMULATION
 
 # Initialize arrays that will be populated with our inputs and states
-x = np.zeros(N)
+x = np.zeros((2, N))
 u = np.zeros(N)
 
-# Set the initial position [m] and initial input [m/s]
-x[0] = 1.0
+# -----------------------------------------------
+# Choose the controller gains
+K = np.array([3.0, 4.0])
+# -----------------------------------------------
+
+# Set the initial position [m], velocity [m/s], and force input [N]
+x[0, 0] = 1.0
+x[1, 0] = 0.0
 u[0] = 0.0
-
-# Set the desired position [m] and controller gain
-x_d = 4.0
-
-# -------------------------------------------------------
-# CHANGE THIS TO TUNE THE CONTROLLER - proportional gain
-k_P = 10
-# -------------------------------------------------------
-
 
 # Run the simulation
 for k in range(1, N):
-    x[k] = vehicle(x[k - 1], u[k - 1], T)
-    u[k] = k_P * (x_d - x[k])
+    x[:, k] = vehicle(x[:, k - 1], u[k - 1], F, G)
+    u[k] = controller(x[:, k], K)
 
-# %%
-# MAKE A PLOT
 
-# Change some plot settings (optional)
-plt.rc("text", usetex=False)
-# plt.rc("text.latex", preamble=r"\usepackage{cmbright,amsmath}")
-plt.rc("savefig", format="pdf")
-plt.rc("savefig", bbox="tight")
-
-# Plot the state (x) and input (u) vs time (t)
+# Plot the states (x) and input (u) vs time (t)
 fig1 = plt.figure(1)
-ax1a = plt.subplot(211)
-plt.plot(t, x, "C0")
+ax1a = plt.subplot(311)
+plt.plot(t, x[0, :], "C0")
 plt.grid(color="0.95")
-plt.ylabel(r"$x$ [m]")
+plt.ylabel(r"$x_1$ [m]")
 plt.setp(ax1a, xticklabels=[])
-ax1b = plt.subplot(212)
+ax1b = plt.subplot(312)
+plt.plot(t, x[1, :], "C0")
+plt.grid(color="0.95")
+plt.ylabel(r"$x_2$ [m/s]")
+plt.setp(ax1b, xticklabels=[])
+ax1c = plt.subplot(313)
 plt.step(t, u, "C1", where="post")
 plt.grid(color="0.95")
-plt.ylabel(r"$u$ [m/s]")
+plt.ylabel(r"$u$ [N]")
 plt.xlabel(r"$t$ [s]")
 
-# Save the plot
-plt.savefig("./oneD_kinematic_control_fig1.pdf")
 
 # %%
 # MAKE AN ANIMATION
@@ -88,9 +94,8 @@ LENGTH = 1.0
 vehicle = Cart(LENGTH)
 
 # Create and save the animation
-ani = vehicle.animate(x, T, True, "./oneD_kinematic_control.gif")
+ani = vehicle.animate(x[0, :], T)
 
-# %%
 
 # Show all the plots to the screen
 plt.show()
